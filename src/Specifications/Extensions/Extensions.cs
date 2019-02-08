7381429281862adc474f11f8783a5e0a0b892dce
Ievgenii Stepanenko <jk_km@ukr.net>
2019-02-08 23:39:29 +0100

    Add extension

---
+++ src/Specifications/Extensions/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Specification.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Specifications.Extensions
+{
+    public static class Extensions
+    {
+        public static IQueryable<T> Where<T>(this IQueryable<T> source, ISpecification<T> spec)
+        {
+            return source.Where(spec.ToExpression());
+        }
+
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> source, ISpecification<T> spec)
+        {
+            return source.Where(spec.IsSatisfiedBy);
+        }
+    }
+}
